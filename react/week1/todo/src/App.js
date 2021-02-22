bf73a9c2d108ea8eadc4865051b847292f8ae0a1
ccshah298 <ccshah298@gmail.com>
2021-02-22 22:25:13 +0100

    todo list added

---
+++ react/week1/todo/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import ToDoItem from './ToDoItem';
+const toDoThing = [
+  { description: "get out of bed", deadline: "wed sep 13 2017" },
+  { description: "Brush Teeth", deadline: "Thu sep 13 2017" },
+  { description: "Eat breakfast", deadline: "wed sep 13 2017" },
+];
+function App() {
+  return (
+    <div className="App">
+   <h2>
+     To do list
+   </h2>
+      <ul>
+          {toDoThing.map(todo => <ToDoItem description={todo.description} deadline={todo.deadline} />)}
+      </ul>
+    </div>
+  );
+}
+
+
+
+export default App;
